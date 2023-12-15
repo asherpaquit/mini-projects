23c44336afaa467abef18e8583bed37c3f9712d2
paxx <85264198+asherpaquit@users.noreply.github.com>
2023-12-15 17:36:49 +0800

    main page created
    with extra js nga bati kaayu hahahahaha

---
+++ bussiness_website/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var choi =  document.getElementById("login");
+    
+    function hover_effect(){
+        alert("goodbye cruel world hehehe");
+    }
+
+    choi.addEventListener("click",hover_effect);
+});
