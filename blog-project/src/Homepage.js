fd17064a37a5246d1b3d065e4e19dcb6a4e6d1cf
paxx <85264198+asherpaquit@users.noreply.github.com>
2024-01-20 22:03:01 +0800

    useState

--- blog-project/src/Homepage.js
+++ blog-project/src/Homepage.js
@@ -1,5 +1,6 @@
 const Homepage = () => {
 
+<<<<<<< Updated upstream
     const handleClick = () => {
         console.log("Hello World");
     }
@@ -8,6 +9,22 @@ const Homepage = () => {
         <div className="content">
             <h1>Homepage</h1>
             <button onClick = {handleClick}>Click Me!</button>
+=======
+   const [blogs, setBlogs] = useState([
+    {title: "My new website", body: "lorem ipsum...", author: "mario", id: 1},
+    {title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2},
+    {title: "Web dev top tips", body: "lorem ipsum...", author: "luigi", id: 3}
+   ])
+
+    return ( 
+        <div className="content">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
+>>>>>>> Stashed changes
         </div>
      );
 }
