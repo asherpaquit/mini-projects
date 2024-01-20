7b721e27d6b38d319da8af6e1295d6f65cf20de9
paxx <85264198+asherpaquit@users.noreply.github.com>
2024-01-20 14:17:28 +0800

    Update Homepage.js

--- blog-project/src/Homepage.js
+++ blog-project/src/Homepage.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+
 const Homepage = () => {
 
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(18);
+
     const handleClick = () => {
-        console.log("Hello World");
+        setName('luigi');
+        setAge(20);
     }
 
     return ( 
         <div className="content">
             <h1>Homepage</h1>
+            <p> {name} is {age} years old </p>
             <button onClick = {handleClick}>Click Me!</button>
         </div>
      );
